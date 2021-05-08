193e7218fb9703c588a16201e8e1af56b3bab330
JinHyungKim <83858427+JinHyungKim@users.noreply.github.com>
2021-05-08 16:15:23 +0900

    Create pr01.java

---
+++ Javapractice/pr01.java
@@ -0,0 +1,12 @@
+package tut_20;
+
+import java.util.Random;
+
+public class Tut_20 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+    }
+}
+
+
+출처: https://studymake.tistory.com/432 [스터디메이크]
