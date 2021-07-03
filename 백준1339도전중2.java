5af70620971684d2b549cac053a7dcfca1107b29
JinHyungKim <83858427+JinHyungKim@users.noreply.github.com>
2021-07-03 23:36:28 +0900

    Create 백준1339도전중2.java

---
+++ 백준1339도전중2.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    String str;
+	    
+	    int asciiArrayIndex = 0;
+	    int count = 9;
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+	    Map<Character, Integer> map = new HashMap<>();
+	    char[][] mainArray = new char[N][8];
+	    int[][] asciiArray = new int[10][2];
+	    for(int j=0;j<N;j++){
+	        str = br.readLine();
+	        for(int i=0;i<8;i++){
+	            for(;i<8-str.length();i++)
+	                mainArray[j][i] = 32;
+	            mainArray[j][i] = str.charAt(i-(8-str.length()));
+	        }
+	    }
+	    for(int j=0;j<8;j++){
+	        for(int currentIndex = 0;currentIndex<N;currentIndex++){
+    	        if(mainArray[currentIndex][j] != 32){
+    	            if(!map.containsKey(mainArray[currentIndex][j]))
+    	                map.put(mainArray[currentIndex][j], count--);
+    	        }
+	        }
+	    }
+	    System.out.println(map);
+	    for(char[] c:mainArray)
+	        System.out.println(c);
+	}
+}
